bb2893fd76765b240faa00caa4a2eef2ea8fbbdb
Jonas Birmé <jonas.birme@eyevinn.se>
2025-06-28 16:38:45 +0200

    fix: support volume for staging dir

--- Dockerfile
+++ Dockerfile
@@ -11,11 +11,15 @@ RUN apk --no-cache add curl
 RUN apk --no-cache add nodejs npm
 RUN apk --no-cache add aws-cli
 RUN apk --no-cache add ffmpeg
-RUN adduser --system --uid 1001 nodejs
+RUN adduser --disabled-password --gecos '' --shell /bin/sh nodejs
 COPY --from=build /source/dist ./dist
 COPY --from=build /source/package.json /source/package-lock.json ./
+COPY --from=build /source/docker-entrypoint.sh /usr/local/bin/docker-entrypoint.sh
+RUN chmod +x /usr/local/bin/docker-entrypoint.sh
 RUN npm ci
 RUN npm install -g .
-USER nodejs
+VOLUME /usercontent
+ENV STAGING_DIR=/usercontent
 
+ENTRYPOINT [ "docker-entrypoint.sh" ]
 CMD ["ffmpeg-s3"]
--- src/cli.ts
+++ src/cli.ts
@@ -1,39 +1,7 @@
 #! /usr/bin/env node
 
-import { Command } from 'commander';
 import { doFFmpeg } from './ffmpeg';
 import { stripQuotes } from './util';
-
-/*
-const cli = new Command();
-cli
-  .description(`Run ffmpeg and output to S3 or local`)
-  .option(
-    '--staging-dir <stagingDir>',
-    'Staging directory (default: /tmp/data)'
-  )
-  .option(
-    '--ffmpeg-executable <ffmpegExecutable>',
-    'Path to ffmpeg executable (default: ffmpeg)'
-  )
-  .argument('<ffmpegCmdString>', 'ffmpeg command string')
-  .action(async (ffmpegCmdString, options) => {
-    try {
-      console.log('Running ffmpeg');
-      await doFFmpeg({
-        cmdString: stripQuotes(ffmpegCmdString),
-        stagingDir: options.stagingDir,
-        ffmpegExecutable:
-          options.ffmpegExecutable || process.env.FFMPEG_EXECUTABLE
-      });
-    } catch (err) {
-      console.log((err as Error).message);
-    }
-  });
-
-cli.parseAsync(process.argv);
-
-*/
 
 async function main(args: string[]) {
   try {
